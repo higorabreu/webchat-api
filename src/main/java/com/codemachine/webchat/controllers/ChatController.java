3d271c9c3cd2d86171cec5b02eaf9d9b0b78e599
higorabreuf@gmail.com <higorabreuf@gmail.com>
2025-04-07 21:21:44 -0300

    [master]chore: rename package

--- src/main/java/com/codemachine/webchat/controllers/ChatController.java
+++ src/main/java/com/codemachine/webchat/controllers/ChatController.java
@@ -4,8 +4,6 @@ import com.codemachine.webchat.service.ChatService;
 import com.codemachine.webchat.domain.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 @Controller
--- src/main/java/com/codemachine/webchat/service/AuthService.java
+++ src/main/java/com/codemachine/webchat/service/AuthService.java
@@ -1,4 +1,4 @@
-package main.java.com.codemachine.webchat.service;
+package com.codemachine.webchat.service;
 
 import com.codemachine.webchat.domain.User;
 import com.codemachine.webchat.domain.UserRepository;
